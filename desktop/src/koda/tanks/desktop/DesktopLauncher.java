00577b7da23849ffa8586d68238101b1022a4fa3
smahbod <smahbod@ucsd.edu>
2014-12-03 19:43:29 -0800

    Initial commit

---
+++ desktop/src/koda/tanks/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package koda.tanks.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import koda.tanks.Tanks;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Tanks(), config);
+	}
+}
